679772e5fe83c44768e0bd0ebb80aa13adfd39a2
Anand Rajaram <anandrajaram21@gmail.com>
2022-03-02 00:10:04 +0530

    Added some code to automatically do stuff, must refactor

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ import RPC from "discord-rpc";
 import jxa from "@jxa/run";
 import axios from "axios";
 
-const client = new RPC.Client({ transport: "ipc" });
+const main = async () => {
+  try {
+    const client = new RPC.Client({ transport: "ipc" });
+    const timer = setInterval(async () => {
+      try {
+        await setActivity(client);
+      } catch (err) {
+        console.error(err);
+        clearInterval(timer);
+        client.destroy();
+        main();
+      }
+    }, 15e3);
+  } catch (err) {
+    console.error(err);
+    setTimeout(main, 15e3);
+  }
+};
+
+const isOpen = async () => {
+  return jxa.run(() => {
+    return Application("System Events").processes["Music"].exists();
+  });
+};
 
 const getState = async () => {
   return jxa.run(() => {
@@ -26,72 +49,47 @@ const fetchArtwork = async (songUrl) => {
   );
 };
 
-const properties = await jxa.run(() => {
-  const music = Application("Music");
-  return {
-    ...music.currentTrack().properties(),
-    playerPosition: music.playerPosition(),
-  };
-});
+const setActivity = async (client) => {
+  const open = await isOpen();
+  if (open) {
+    const state = await getState();
+    switch (state) {
+      case "playing": {
+        const properties = await jxa.run(() => {
+          const music = Application("Music");
+          return {
+            ...music.currentTrack().properties(),
+            playerPosition: music.playerPosition(),
+          };
+        });
+        const songUrl = await fetchSongUrl();
+        const artwork = await fetchArtwork(songUrl);
+        const delta = (properties.duration - properties.playerPosition) * 1000;
+        const end = Math.ceil(Date.now() + delta);
 
-const songUrl = await fetchSongUrl();
-const artwork = await fetchArtwork(songUrl);
-const delta = (properties.duration - properties.playerPosition) * 1000;
-const end = Math.ceil(Date.now() + delta);
+        const activity = {
+          details: properties.name,
+          state: `${properties.artist} — ${properties.album}`,
+          endTimestamp: end,
+          largeImageKey: artwork.data.thumb,
+        };
 
-const activity = {
-  details: properties.name,
-  state: `${properties.artist} — ${properties.album}`,
-  timestamps: { end },
-  assets: {
-    large_image: artwork.data.thumb,
-    large_text: properties.name,
-  },
+        client.on("ready", async () => {
+          client.setActivity(activity);
+        });
+
+        break;
+      }
+
+      default: {
+        await client.clearActivity();
+        break;
+      }
+    }
+    client.login({ clientId: "947831155376422923" });
+  } else {
+    await client.clearActivity();
+  }
 };
 
-client.on("ready", async () => {
-  await client.request("SET_ACTIVITY", {
-    pid: process.pid,
-    activity: activity,
-  });
-  console.log("done");
-});
-
-client.login({ clientId: "947831155376422923" });
-
-// jxa
-// .run(() => {
-// const music = Application("Music");
-// return {
-// ...music.currentTrack().properties(),
-// playerPosition: music.playerPosition(),
-// };
-// })
-// .then((properties) => {
-// fetchSongUrl().then((res) => {
-// fetchArtwork(res).then((res) => {
-// const delta = (properties.duration - properties.playerPosition) * 1000;
-// const end = Math.ceil(Date.now() + delta);
-// const activity = {
-// details: properties.name,
-// state: `${properties.artist} — ${properties.album}`,
-// timestamps: { end: end },
-// assets: {
-// large_image: res.data.thumb,
-// large_text: "Song",
-// },
-// instance: true,
-// };
-
-// client.on("ready", () => {
-// client.request("SET_ACTIVITY", {
-// pid: process.pid,
-// activity: activity,
-// });
-// console.log("Done");
-// });
-
-// client.login({ clientId: "947831155376422923" });
-// });
-// });
-// });
+main();
