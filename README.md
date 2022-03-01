6c63d5974830afba519aecf1eb8e4047e9659e80
Anand Rajaram <anandrajaram21@gmail.com>
2022-03-01 23:17:43 +0530

    Refactor a lot of code to use ES Modules, and Top Level Await The only thing left now is to get the current song's url

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# discord-rpc-apple-music
+# Apple Music Discord RPC
 Discord RPC Client for Apple Music
+
+
+## Disclaimer
+
+A lot of the techniques used here have taken inspiration heavily from [this repo](https://github.com/NextFire/apple-music-discord-rpc). A lot of what I did wouldn't have been possible without the ideas from their particular implementation. 
+
+## Why
+
+In the repo mentioned before, I faced many issues with the album artwork not being the right one, and that kinda annoyed me. So I thought there might be a better way to do the same things that they did in the original repo. 
+
+## Some Problems
+
+Getting the artwork is the biggest problem, everything else is relatively straightforward. There is no way to get the URL of the current song playing (atleast from my research) through AppleScript (or JXA, Javascript for Automation, which is Apple's AppleScript implementation but in Javascript)
+
+## Installation
+
+Will be updated once I get it installed properly myself
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const RPC = require("discord-rpc");
-const jxa = require("@jxa/run");
-const axios = require("axios");
+import RPC from "discord-rpc";
+import jxa from "@jxa/run";
+import axios from "axios";
 
 const client = new RPC.Client({ transport: "ipc" });
 
+const getState = async () => {
+  return jxa.run(() => {
+    return Application("Music").playerState();
+  });
+};
+
 const fetchSongUrl = async () => {
-  return "https://music.apple.com/in/album/ripples-in-the-sand/1583651332?i=1583651917";
+  return "https://music.apple.com/in/album/legend/1440909016?i=1440909028";
 };
 
 const fetchArtwork = async (songUrl) => {
@@ -20,32 +26,72 @@ const fetchArtwork = async (songUrl) => {
   );
 };
 
-jxa
-  .run(() => {
-    return Application("Music").currentTrack().properties();
-  })
-  .then((properties) => {
-    fetchSongUrl().then((res) => {
-      fetchArtwork(res).then((res) => {
-        const activity = {
-          details: properties.name,
-          assets: {
-            large_image: res.data.thumb,
-            large_text: "Song",
-          },
-          timestamps: { start: Date.now() },
-          instance: true,
-        };
+const properties = await jxa.run(() => {
+  const music = Application("Music");
+  return {
+    ...music.currentTrack().properties(),
+    playerPosition: music.playerPosition(),
+  };
+});
 
-        client.on("ready", () => {
-          client.request("SET_ACTIVITY", {
-            pid: process.pid,
-            activity: activity,
-          });
-          console.log("Done");
-        });
+const songUrl = await fetchSongUrl();
+const artwork = await fetchArtwork(songUrl);
+const delta = (properties.duration - properties.playerPosition) * 1000;
+const end = Math.ceil(Date.now() + delta);
 
-        client.login({ clientId: "947831155376422923" });
-      });
-    });
+const activity = {
+  details: properties.name,
+  state: `${properties.artist} — ${properties.album}`,
+  timestamps: { end },
+  assets: {
+    large_image: artwork.data.thumb,
+    large_text: properties.name,
+  },
+};
+
+client.on("ready", async () => {
+  await client.request("SET_ACTIVITY", {
+    pid: process.pid,
+    activity: activity,
   });
+  console.log("done");
+});
+
+client.login({ clientId: "947831155376422923" });
+
+// jxa
+// .run(() => {
+// const music = Application("Music");
+// return {
+// ...music.currentTrack().properties(),
+// playerPosition: music.playerPosition(),
+// };
+// })
+// .then((properties) => {
+// fetchSongUrl().then((res) => {
+// fetchArtwork(res).then((res) => {
+// const delta = (properties.duration - properties.playerPosition) * 1000;
+// const end = Math.ceil(Date.now() + delta);
+// const activity = {
+// details: properties.name,
+// state: `${properties.artist} — ${properties.album}`,
+// timestamps: { end: end },
+// assets: {
+// large_image: res.data.thumb,
+// large_text: "Song",
+// },
+// instance: true,
+// };
+
+// client.on("ready", () => {
+// client.request("SET_ACTIVITY", {
+// pid: process.pid,
+// activity: activity,
+// });
+// console.log("Done");
+// });
+
+// client.login({ clientId: "947831155376422923" });
+// });
+// });
+// });
